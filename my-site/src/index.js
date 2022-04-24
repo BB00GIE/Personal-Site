bae852aee69cda01d3acd584b65f4a1899970a61
Brandon Elmore <47788498+BB00GIE@users.noreply.github.com>
2022-04-24 03:06:27 -0400

    test2

--- my-site/src/index.js
+++ my-site/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>I did a thing hehehehehehehe</div>
+    <div>I did a thing hehehehehehehehe</div>
     <App />
   </React.StrictMode>
 );
