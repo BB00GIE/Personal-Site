7619062ee7d34f491a4969bd1e7625cbf901e911
Brandon Elmore <47788498+BB00GIE@users.noreply.github.com>
2022-04-25 00:33:22 -0400

    NavBar Testing

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+    return (
+      <div className="NavBar">
+        <header className="NavBar-header">
+          <p>
+            Im the navBar
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>I did a thing hehehehehehehehehe</div>
+    <NavBar />
     <App />
   </React.StrictMode>
 );
